dcac4538b4092a9c185daff92acbfd12a44bc4dd
Christian Mayer <christian@fox21.at>
2014-07-29 15:25:50 +0200

    ZendMailStorage test added.

---
+++ tests/ZendMailStorageTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use Zend\Mail\Storage;
+
+class ZendMailStorageTest extends PHPUnit_Framework_TestCase{
+	
+	public function providerFlags(){
+		$rv = array();
+		$rv[] = array(Storage::FLAG_SEEN, '\Seen');
+		$rv[] = array(Storage::FLAG_ANSWERED, '\Answered');
+		$rv[] = array(Storage::FLAG_FLAGGED, '\Flagged');
+		$rv[] = array(Storage::FLAG_DELETED, '\Deleted');
+		$rv[] = array(Storage::FLAG_DRAFT, '\Draft');
+		$rv[] = array(Storage::FLAG_RECENT, '\Recent');
+		return $rv;
+	}
+	
+	/**
+     * @dataProvider providerFlags
+     */
+	public function testFlags($flag, $expect){
+		$this->assertEquals($expect, $flag);
+	}
+	
+}
