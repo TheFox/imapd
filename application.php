f149187ecaf11caeabd97f9a29e1b644d94f156e
Christian Mayer <christian@fox21.at>
2014-08-05 17:20:24 +0200

    Dev version.

--- application.php
+++ application.php
@@ -7,6 +7,6 @@ use Symfony\Component\Console\Application;
 
 use TheFox\Console\Command\ServerCommand;
 
-$application = new Application('IMAPd', '0.1.0');
+$application = new Application('IMAPd', '0.2.0-dev');
 $application->add(new ServerCommand());
 $application->run();
