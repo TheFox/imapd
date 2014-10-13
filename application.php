5bb9848a245db60738cc2a03a06b776dc18bfc44
Christian Mayer <christian@fox21.at>
2014-10-13 21:46:14 +0200

    Info version added.

--- application.php
+++ application.php
@@ -5,8 +5,11 @@ require_once __DIR__.'/bootstrap.php';
 
 use Symfony\Component\Console\Application;
 
+use TheFox\Console\Command\InfoCommand;
 use TheFox\Console\Command\ServerCommand;
+use TheFox\Imap\Imapd;
 
-$application = new Application('IMAPd', '0.2.x-dev');
+$application = new Application(Imapd::NAME, Imapd::VERSION);
+$application->add(new InfoCommand());
 $application->add(new ServerCommand());
 $application->run();
---
+++ src/TheFox/Imap/Imapd.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace TheFox\Imap;
+
+class Imapd{
+	
+	const NAME = 'IMAPd';
+	const VERSION = '0.2.x-dev';
+	const RELEASE = 4;
+	
+}
